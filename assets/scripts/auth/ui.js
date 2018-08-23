cb2fbe54fd346f2fc28836484143a6c67cc6f38c
mrmcgown <mrmcgown@gmail.com>
2018-08-23 11:45:08 -0400

    Update styling
    Navbar now has correct formatting and auth functionality.
    Need to revise modal styling.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,67 +1,101 @@
-'use strict'
+"use strict";
 
-const store = require('../store')
+const store = require("../store");
 
-const signUpSuccess = function (data) {
-  $('#message').text('Signed up successfully').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'green')
-  $('#sign-up')[0].reset()
-  $('#signUp').modal('hide')
-  console.log('signUpSuccess ran. Data is :', data)
-}
+const signUpSuccess = function(data) {
+  $("#message")
+    .text("Signed up successfully")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "green");
+  $("#sign-up")[0].reset();
+  $("#signUp").modal("hide");
+  console.log("signUpSuccess ran. Data is :", data);
+};
 
-const signUpFailure = function (error) {
-  $('#message').text('Error on sign up').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'red')
-  console.log('signUpFailure ran. Error is :', error)
-}
+const signUpFailure = function(error) {
+  $("#message")
+    .text("Error on sign up")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "red");
+  console.log("signUpFailure ran. Error is :", error);
+};
 
-const signInSuccess = function (data) {
+const signInSuccess = function(data) {
   // $('#message').html('<div class="alert alert-success"><strong>Success!</strong> You should <a href="#" class="alert-link">read this message</a>.</div>').show()
-  $('#message').text('Signed in successfully').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'green')
-  $('#sign-in')[0].reset()
-  $('#sign-out, #changePassButton, #options').show()
-  $('#table, #signUpButton, #signInButton').hide()
-  $('#signIn').modal('hide')
-  console.log('signInSuccess ran. Data is :', data)
-  store.user = data.user
-}
+  $("#message")
+    .text("Signed in successfully")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "green");
+  $("#sign-in")[0].reset();
+  $("#sign-out, #changePassButton, #options").show();
+  $("#table, #signUpButton, #signInButton").hide();
+  $("#signIn").modal("hide");
+  console.log("signInSuccess ran. Data is :", data);
+  store.user = data.user;
+};
 
-const signInFailure = function (error) {
-  $('#message').text('Error on sign in').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'red')
-  console.log('signInFailure ran. Error is :', error)
-}
+const signInFailure = function(error) {
+  $("#message")
+    .text("Error on sign in")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "red");
+  console.log("signInFailure ran. Error is :", error);
+};
 
-const changePasswordSuccess = function (data) {
-  $('#message').text('Changed password successfully').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'green')
-  $('#change-password')[0].reset()
-  $('#changePassword').modal('hide')
-}
+const changePasswordSuccess = function(data) {
+  $("#message")
+    .text("Changed password successfully")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "green");
+  $("#change-password")[0].reset();
+  $("#changePassword").modal("hide");
+};
 
-const changePasswordFailure = function (error) {
-  $('#message').text('Error on change password').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'red')
-  console.log('changePasswordFailure ran. Error is :', error)
-}
+const changePasswordFailure = function(error) {
+  $("#message")
+    .text("Error on change password")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "red");
+  console.log("changePasswordFailure ran. Error is :", error);
+};
 
-const signOutSuccess = function () {
-  $('#message').text('Signed out successfully').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'green')
-  $('#table').find('tr:gt(0)').remove()
-  $('#table, #table-headers, .chore-changes, #sign-out, #changePassButton, #options, #chores-search, #add-chore, #delete-chore, #update-chore').hide()
-  $('#signUpButton, #signInButton').show()
+const signOutSuccess = function() {
+  $("#message")
+    .text("Signed out successfully")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "green");
+  $("#table")
+    .find("tr:gt(0)")
+    .remove();
+  $("#sign-out, #changePassButton, #options").hide();
+  $("#signUpButton, #signInButton").show();
   // $('#content').html('')
-  store.user = null
-}
+  store.user = null;
+};
 
-const signOutFailure = function (error) {
-  $('#message').text('Error on sign out').show().delay(1500).fadeOut()
-  $('#message').css('background-color', 'red')
-  console.log('signOutFailure ran. Error is :', error)
-}
+const signOutFailure = function(error) {
+  $("#message")
+    .text("Error on sign out")
+    .show()
+    .delay(1500)
+    .fadeOut();
+  $("#message").css("background-color", "red");
+  console.log("signOutFailure ran. Error is :", error);
+};
 
 module.exports = {
   signUpSuccess,
@@ -72,4 +106,4 @@ module.exports = {
   changePasswordFailure,
   signOutFailure,
   signOutSuccess
-}
+};
