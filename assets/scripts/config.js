7b00af5b8b8f0a06564b664d8e915da26db7d79c
mrmcgown <mrmcgown@gmail.com>
2018-09-07 14:29:38 -0400

    Fix production API url link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://still-hollows-79663.herokuapp.com/',
+  production: 'https://still-hollows-79663.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
